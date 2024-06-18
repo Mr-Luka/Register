56cf88ffcc198ae45d0e67273e9fa4875a80ab65
Luka <luka.luka.luka93@gmail.com>
2024-06-18 14:17:04 -0700

    called all the necessary elements, added event listener to the button and started working on the handleInputClick

--- js/script.js
+++ js/script.js
@@ -1,21 +1,18 @@
-const name = document.querySelector(".name");
-const email = document.querySelector(".email");
+const nameFull = document.querySelector(`#name`);
+const email = document.querySelector(`#email`);
 const buttonContinue = document.querySelector(".continue");
 const container1 = document.querySelector(".container");
 const container2 = document.querySelector(".container2");
 const container3 = document.querySelector(".container2");
 
+let nameVal;
+let emailVal;
 
-const handleInputClick = (e) => {
-    const input = e.target.value;
-    const validName = /^[a-z ,.'-]+$/i;
-    const validEmail = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-    if (input !== validName || input !== validEmail) {
-        alert("Please fill out the fileds correctly")
-    } else {
-        container1.classList.add("hidden");
-        container2.classList.remove("hidden");
-    }
+const handleInputClick = () => {
+    nameVal = nameFull.value;
+    emailVal = email.value;
+    const correctName = /^[a-z ,.'-]+$/i;
+    const correctEmail = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 } 
 
 buttonContinue.addEventListener("click", handleInputClick);
